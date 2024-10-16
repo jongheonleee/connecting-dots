3dd99825a36cf8606c7d6321e4b4b26061041a5e
jongheonleee <qwefghnm1212@gmail.com>
2024-10-16 18:03:23 +0900

    [feat] 게시글 이미지 테스트 환경 구성

--- src/main/java/com/example/demo/dao/BoardImgDaoImpl.java
+++ src/main/java/com/example/demo/dao/BoardImgDaoImpl.java
@@ -1,5 +1,7 @@
 package com.example.demo.dao;
 
+import com.example.demo.dto.BoardImgFormDto;
+import java.util.List;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.retry.annotation.Recover;
@@ -17,5 +19,38 @@ public class BoardImgDaoImpl {
     public int count() {
         return session.selectOne(namespace + "count");
     }
+
+   public int insert(BoardImgFormDto dto) {
+        return session.insert(namespace + "insert", dto);
+   }
+
+   public List<BoardImgFormDto> selectAllByBno(int bno) {
+        return session.selectList(namespace + "selectAllByBno", bno);
+   }
+
+   public BoardImgFormDto selectByIno(int ino) {
+        return session.selectOne(namespace + "selectByIno", ino);
+   }
+
+   public List<BoardImgFormDto> selectAll() {
+        return session.selectList(namespace + "selectAll");
+   }
+
+   public int update(BoardImgFormDto dto) {
+        return session.update(namespace + "update", dto);
+   }
+
+   public int deleteByBno(int bno) {
+        return session.delete(namespace + "deleteByBno", bno);
+   }
+
+   public int deleteByIno(int ino) {
+        return session.delete(namespace + "deleteByIno", ino);
+   }
+
+   public int deleteAll() {
+        return session.delete(namespace + "deleteAll");
+   }
+
 
 }
