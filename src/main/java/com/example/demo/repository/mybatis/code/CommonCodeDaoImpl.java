80a67d8d864c360fe8e25a601d5a123344c60ad2
jongheonleee <qwefghnm1212@gmail.com>
2025-01-07 23:43:27 +0900

    [chore] 통합 코드 임시 테스트 코드 추가

--- src/main/java/com/example/demo/repository/mybatis/code/CommonCodeDaoImpl.java
+++ src/main/java/com/example/demo/repository/mybatis/code/CommonCodeDaoImpl.java
@@ -1,5 +1,6 @@
 package com.example.demo.repository.mybatis.code;
 
+import com.example.demo.dto.code.CodeRequestDto;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -10,7 +11,18 @@ public class CommonCodeDaoImpl {
     @Autowired
     private SqlSession session;
 
-    private static final String namespace = "com.example.demo.mapper.CommonCodeMapper.";
+    private static final String namespace = "com.example.demo.mapper.code.CommonCodeMapper.";
 
+    public int count() {
+        return session.selectOne(namespace + "count");
+    }
+
+    public int insert(CodeRequestDto dto) {
+        return session.insert(namespace + "insert", dto);
+    }
+
+    public int deleteByLevel(Integer level) {
+        return session.delete(namespace + "deleteByLevel", level);
+    }
 
 }
---
+++ src/test/java/com/example/demo/repository/mybatis/code/CommonCodeDaoImplTest.java
@@ -0,0 +1,55 @@
+package com.example.demo.repository.mybatis.code;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.example.demo.dto.code.CodeRequestDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonCodeDaoImplTest {
+
+    private final Integer MAX_LEVEL = 3;
+
+    @Autowired
+    private CommonCodeDaoImpl commonCodeDaoImpl;
+
+    @BeforeEach
+    void setUp() {
+        assertNotNull(commonCodeDaoImpl);
+        for (int i=MAX_LEVEL; i>=0; i--) {
+            commonCodeDaoImpl.deleteByLevel(i);
+        }
+    }
+
+
+    @DisplayName("카운팅 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 5, 10, 15, 20})
+    void testCount(int cnt) {
+        int count = commonCodeDaoImpl.count();
+        assertEquals(0, count);
+    }
+
+    @DisplayName("생성 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 5, 10, 15, 20})
+    void testInsert(int cnt) {
+         CodeRequestDto dto = new CodeRequestDto();
+         dto.setLevel(1);
+         dto.setCode("code");
+         dto.setName("name");
+         dto.setChk_use("Y");
+         dto.setReg_date("2021-01-01");
+         dto.setReg_user_seq(1);
+         dto.setUp_user_seq(1);
+         dto.setUp_date("2021-01-01");
+         int result = commonCodeDaoImpl.insert(dto);
+         assertEquals(1, result);
+    }
+}
