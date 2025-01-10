3b3b0819c809ef52dde3cb7266f75bb2dd0e3331
jongheonleee <qwefghnm1212@gmail.com>
2025-01-10 12:04:19 +0900

    [feat] code, service_rule_use 페이지네이션을 위한 클래스 정의

---
+++ src/main/java/com/example/demo/dto/code/CodePageResponse.java
@@ -0,0 +1,44 @@
+package com.example.demo.dto.code;
+
+
+import com.example.demo.dto.SearchCondition;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+public class CodePageResponse {
+
+    private static final Integer DEFAULT_PAGE = 1;
+    private static final Integer DEFAULT_PAGE_SIZE = 10;
+    private static final Integer MAX_PAGE_SIZE = 100;
+    private static final Integer MIN_PAGE_SIZE = 10;
+
+    private Integer totalCnt;
+    private Integer page;
+    private Integer pageSize;
+    private Integer totalPage;
+    private List<CodeResponse> responses;
+
+    private CodePageResponse(Integer totalCnt, SearchCondition sc, List<CodeResponse> responses) {
+        this.totalCnt = totalCnt;
+        this.page = sc.getPage() == null ? DEFAULT_PAGE : sc.getPage();
+        this.pageSize = sc.getPageSize() == null ? DEFAULT_PAGE_SIZE : sc.getPageSize();
+        this.pageSize = this.pageSize > MAX_PAGE_SIZE ? MAX_PAGE_SIZE : this.pageSize;
+        this.pageSize = this.pageSize < MIN_PAGE_SIZE ? MIN_PAGE_SIZE : this.pageSize;
+        totalPage = (totalCnt / sc.getPageSize()) + (totalCnt % sc.getPageSize() == 0 ? 0 : 1);
+        this.responses = responses;
+    }
+
+
+    public static CodePageResponse of(Integer totalCnt, SearchCondition sc, List<CodeResponse> responses) {
+        return new CodePageResponse(totalCnt, sc, responses);
+    }
+
+}
---
+++ src/main/java/com/example/demo/dto/service/ServiceRuleUserPageResponse.java
@@ -0,0 +1,42 @@
+package com.example.demo.dto.service;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class ServiceRuleUserPageResponse {
+
+    private static final Integer DEFAULT_PAGE = 1;
+    private static final Integer DEFAULT_PAGE_SIZE = 10;
+    private static final Integer MAX_PAGE_SIZE = 100;
+    private static final Integer MIN_PAGE_SIZE = 10;
+
+    private Integer totalCnt;
+    private Integer page;
+    private Integer pageSize;
+    private Integer totalPage;
+    private List<ServiceRuleUseResponse> responses;
+
+    private ServiceRuleUserPageResponse(Integer totalCnt, Integer page, Integer pageSize, List<ServiceRuleUseResponse> responses) {
+        this.totalCnt = totalCnt;
+        this.page = page == null ? DEFAULT_PAGE : page;
+        this.pageSize = pageSize == null ? DEFAULT_PAGE_SIZE : pageSize;
+        this.pageSize = this.pageSize > MAX_PAGE_SIZE ? MAX_PAGE_SIZE : this.pageSize;
+        this.pageSize = this.pageSize < MIN_PAGE_SIZE ? MIN_PAGE_SIZE : this.pageSize;
+        totalPage = (totalCnt / pageSize) + (totalCnt % pageSize == 0 ? 0 : 1);
+        this.responses = responses;
+    }
+
+    public static ServiceRuleUserPageResponse of(Integer totalCnt, Integer page, Integer pageSize, List<ServiceRuleUseResponse> responses) {
+        return new ServiceRuleUserPageResponse(totalCnt, page, pageSize, responses);
+    }
+
+}
