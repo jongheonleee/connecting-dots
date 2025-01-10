dde0246eb115214713791f3646982186483c373f
jongheonleee <qwefghnm1212@gmail.com>
2025-01-10 22:56:14 +0900

    [feat] service_terms : service 구현

--- src/test/java/com/example/demo/application/service/ServiceTermsServiceImplTest.java
+++ src/test/java/com/example/demo/application/service/ServiceTermsServiceImplTest.java
@@ -2,6 +2,8 @@ package com.example.demo.application.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.example.demo.repository.mybatis.service.ServiceTermsDaoImpl;
+import com.example.demo.utils.CustomFormatter;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -13,6 +15,14 @@ class ServiceTermsServiceImplTest {
 
     @InjectMocks
     private ServiceTermsServiceImpl serviceTermsService;
+
+    @Mock
+    private ServiceTermsDaoImpl serviceTermsDao;
+
+    @Mock
+    private CustomFormatter formatter;
+
+
     
 
 }
