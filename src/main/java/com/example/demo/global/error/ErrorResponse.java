1f1bc099bb1879833b2c187156339b52cdea926b
jongheonleee <qwefghnm1212@gmail.com>
2025-01-14 14:42:31 +0900

    [feat] GlobalExceptionHandler를 통한 공통 예외 처리

--- src/main/java/com/example/demo/global/error/ErrorResponse.java
+++ src/main/java/com/example/demo/global/error/ErrorResponse.java
@@ -1,9 +1,15 @@
 package com.example.demo.global.error;
 
+import com.example.demo.global.error.exception.ErrorCode;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @Getter
 @NoArgsConstructor
@@ -14,11 +20,69 @@ public class ErrorResponse {
     private List<FieldError> errors;
     private String code;
 
-    private ErrorResponse(String message, int status, List<FieldError> errors, String code) {
-        this.message = message;
-        this.status = status;
+
+    private ErrorResponse(ErrorCode code, List<FieldError> errors) {
+        this.message = code.getMessage();
+        this.status = code.getStatus();
         this.errors = errors;
-        this.code = code;
+        this.code = code.getCode();
+    }
+
+    private ErrorResponse(ErrorCode code) {
+        this.message = code.getMessage();
+        this.status = code.getStatus();
+        this.code = code.getCode();
+        this.errors = new ArrayList<>();
+    }
+
+
+    public static ErrorResponse of(ErrorCode code, BindingResult bindingResult) {
+        return new ErrorResponse(code, FieldError.of(bindingResult));
+    }
+
+    public static ErrorResponse of(ErrorCode code) {
+        return new ErrorResponse(code);
+    }
+
+    public static ErrorResponse of(ErrorCode code, List<FieldError> errors) {
+        return new ErrorResponse(code, errors);
+    }
+
+    public static ErrorResponse of(MethodArgumentTypeMismatchException e) {
+        final String value = e.getValue() == null ? "" : e.getValue().toString();
+        final List<ErrorResponse.FieldError> errors = ErrorResponse.FieldError.of(e.getName(), value, e.getErrorCode());
+        return new ErrorResponse(ErrorCode.INVALID_TYPE_VALUE, errors);
+    }
+
+
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class FieldError {
+        private String field;
+        private String value;
+        private String reason;
+
+        private FieldError(String field, String value, String reason) {
+            this.field = field;
+            this.value = value;
+            this.reason = reason;
+        }
+
+        public static List<FieldError> of(String field, String value, String reason) {
+            List<FieldError> fieldErrors = new ArrayList<>();
+            fieldErrors.add(new FieldError(field, value, reason));
+            return fieldErrors;
+        }
+
+        private static List<FieldError> of(BindingResult bindingResult) {
+            final List<org.springframework.validation.FieldError> fieldErrors = bindingResult.getFieldErrors();
+            return fieldErrors.stream()
+                    .map(error -> new FieldError(
+                            error.getField(),
+                            error.getRejectedValue() == null ? "" : error.getRejectedValue().toString(),
+                            error.getDefaultMessage()))
+                    .collect(Collectors.toList());
+        }
     }
 
 }
--- src/main/java/com/example/demo/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/global/error/GlobalExceptionHandler.java
@@ -1,9 +1,110 @@
 package com.example.demo.global.error;
 
+import com.example.demo.global.error.exception.ErrorCode;
+import com.example.demo.global.error.exception.business.BusinessException;
+import com.example.demo.global.error.exception.technology.TechnicalException;
+import java.nio.file.AccessDeniedException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.validation.BindException;
+
 
 // 모든 예외 공통적으로 처리
-@ControllerAdvice
+@Slf4j
+@ControllerAdvice(basePackages = {"com.example.demo.presentation"})
 public class GlobalExceptionHandler {
+
+    /**
+     *  - javax.validation.Valid or @Validated 으로 binding error 발생시 발생함.
+     *  - HttpMessageConverter 에서 등록한 HttpMessageConverter binding 못할경우 발생함.
+     *  -> 주로 @RequestBody, @RequestPart 어노테이션에서 발생
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.error("handleMethodArgumentNotValidException", e);
+        ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE, e.getBindingResult());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    /**
+     * - @ModelAttribut 으로 binding error 발생시 BindException 발생함.
+     */
+    @ExceptionHandler(BindException.class)
+    protected ResponseEntity<ErrorResponse> handleBindException(BindException e) {
+        log.error("handleBindException", e);
+        ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE, e.getBindingResult());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    /**
+     * - enum type 일치하지 않아 binding 못할 경우 발생함.
+     * -> 주로 @RequestParam enum으로 binding 못했을 경우 발생함.
+     */
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    protected ResponseEntity<ErrorResponse> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e) {
+        log.error("handleMethodArgumentTypeMismatchException", e);
+        ErrorResponse response = ErrorResponse.of(e);
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    /**
+     * - 지원하지 않은 HTTP method 호출 할 경우 발생함.
+     */
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    protected ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
+        log.error("handleHttpRequestMethodNotSupportedException", e);
+        ErrorResponse response = ErrorResponse.of(ErrorCode.METHOD_NOT_ALLOWED);
+        return new ResponseEntity<>(response, HttpStatus.METHOD_NOT_ALLOWED);
+    }
+
+    /**
+     * - Authentication 객체가 필요한 권한을 보유하지 않은 경우 발생함.
+     */
+    @ExceptionHandler(AccessDeniedException.class)
+    protected ResponseEntity<ErrorResponse> handleAccessDeniedException(AccessDeniedException e) {
+        log.error("handleAccessDeniedException", e);
+        ErrorResponse response = ErrorResponse.of(ErrorCode.HANDLE_ACCESS_DENIED);
+        return new ResponseEntity<>(response, HttpStatus.valueOf(ErrorCode.HANDLE_ACCESS_DENIED.getStatus()));
+    }
+
+    /**
+     * - 비즈니스 로직 수행 중 발생한 예외를 처리
+     */
+    @ExceptionHandler(BusinessException.class)
+    protected ResponseEntity<ErrorResponse> handleBusinessException(final BusinessException e) {
+        log.error("handleEntityNotFoundException", e);
+        ErrorCode errorCode = e.getErrorCode();
+        ErrorResponse response = ErrorResponse.of(errorCode);
+        return new ResponseEntity<>(response, HttpStatus.valueOf(errorCode.getStatus()));
+    }
+
+
+    /**
+     * - 기술적인 예외를 처리
+     */
+    @ExceptionHandler(TechnicalException.class)
+    protected ResponseEntity<ErrorResponse> handleTechnicalException(final TechnicalException e) {
+        log.error("handleEntityNotFoundException", e);
+        ErrorCode errorCode = e.getErrorCode();
+        ErrorResponse response = ErrorResponse.of(errorCode);
+        return new ResponseEntity<>(response, HttpStatus.valueOf(errorCode.getStatus()));
+    }
+
+
+    /**
+     * - 나머지 모든 예외를 처리
+     */
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<ErrorResponse> handleException(Exception e) {
+        log.error("handleEntityNotFoundException", e);
+        ErrorResponse response = ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
 }
